df2c3f3e0577d59a16bf320145e256f6cf32aef7
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-31 01:43:28 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,10 @@ def my_select(array)
   select = []
   
   while i < array.length
-    #Some kind of yield method
+    if yield(array[i])
+      select << array[i]
+    end
     i += 1
   end
+  select
 end
